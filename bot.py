5cf60004bcd5c378f4f823a125f3a9f304d2a953
Nick De Villiers <n.devilliers1999@gmail.com>
2020-12-01 23:09:38 +0000

    Connected servers are printed in terminal on init.

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ import random
 config = configparser.ConfigParser()
 config.read("iain.cfg")
 
+# gets the bot token
 TOKEN = config["INFO"]["token"]
 IAIN_PUNS = [
         "Don't interrupt someone working intently on a puzzle. Chances are, you'll hear some crosswords!",
@@ -19,24 +20,33 @@ IAIN_PUNS = [
         "The machine at the coin factory just suddenly stopped working, with no explanation. It doesn't make any cents!",
         "Yesterday, a clown held the door open for me. It was such a nice jester!",
         "I'm only friends with 25 letters of the alphabet. I don't know Y."
-    ]
+    ] # yes, this is indeed a list of puns.
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="!") #setting the command prefix to !
 
+# bot initialisation readouts
 @bot.event
 async def on_ready():
     # guild = utils.find(lambda g: g.name == GUILD, bot.guilds)
     print(
         # f'{bot.user.name} is connected to the following guild:\n'
         # f'{guild.name}(id: {guild.id})'
-        f'{bot.user.name} is connected to discord!'
+        f'{bot.user.name} is live.\n'
+        f'{bot.user.name} is connected to:'
         )
+    for server in bot.guilds:
+        print(
+            f'-{server.name}'
+            )
+    print("")
 
+# prints a random pun from the pun list
 @bot.command(name="pun")
 async def pun(ctx):
     response = random.choice(IAIN_PUNS)
     await ctx.send(response)
 
+# prints "No worries!" with an image of ronald murray smiling. How lovely
 @bot.command(name="thanks")
 async def pun(ctx):
     response = "No worries!"
