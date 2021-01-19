13439f69758f93e730df3b15d33aa5ba818ca7ba
Max <max.fyall@hotmail.co.uk>
2021-01-19 22:41:36 +0000

    another typo... smh. I should really double check things ¯\_(ツ)_/¯

--- anecdotes.py
+++ anecdotes.py
@@ -5,7 +5,7 @@ neutral = [
     "I remember in 2000 when we thought that duel-core processors was way more than we ever need..."
     "I always loved listening to my CRT screen make the high pitch noise everytime it was on..."
     "Let me tell you about Designing a relational database, it's very interesting...",
-    "Did I ever tell you that I once got an autograph from Edgar F. 'Ted' Codd. I keep it on my mantelpeice",
+    "Did I ever tell you that I once got an autograph from Edgar F. 'Ted' Codd. I keep it on my mantelpiece",
     "Concorde was the best peice of aviation engineering in history, why did it have to be retired...",
     "I was once on television talking about the damn busters in World War 2, I think I might be a national celebrity... "
      
